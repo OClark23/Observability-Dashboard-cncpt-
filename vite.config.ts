c06bd3945561254648d718a98380c89d4640e792
Warren Clark <59663085+OClark23@users.noreply.github.com>
2025-06-07 08:22:10 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: path.resolve(__dirname, './'),
   build: {
     rollupOptions: {
       input: 'index.html'
